2f11604ae60aad609129a51b608e67b9537d0022
suicedek <handsomefox.hf@gmail.com>
2022-01-29 10:34:48 +0200

    Store metadata strings as QStrings.

--- src/Metadata.h
+++ src/Metadata.h
@@ -7,15 +7,15 @@
 struct Metadata {
   Metadata() = default;
   explicit Metadata(const QMediaMetaData &metadata) {
-    Artist = metadata.value(QMediaMetaData::AlbumArtist).toString().toStdWString();
-    Title = metadata.value(QMediaMetaData::Title).toString().toStdWString();
+    Artist = metadata.value(QMediaMetaData::AlbumArtist).toString();
+    Title = metadata.value(QMediaMetaData::Title).toString();
     Milliseconds = metadata.value(QMediaMetaData::Duration).toInt();
     Seconds = (Milliseconds / 1000) % 60;
     Minutes = ((Milliseconds / 1000) - Seconds) / 60;
     Cover = metadata.value(QMediaMetaData::ThumbnailImage).value<QImage>();
   }
-  std::wstring Artist;
-  std::wstring Title;
+  QString Artist;
+  QString Title;
   int Milliseconds{};
   int Seconds{};
   int Minutes{};
